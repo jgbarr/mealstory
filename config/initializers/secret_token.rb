9cd13d6ce228864f7d3d60c1de9130557d11b0a3
Jeff Barr <jeff@jeffbarr.com>
2011-04-09 11:42:53 -0700

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mealstory::Application.config.secret_token = 'c49de90a8cd07d4b02dc6cad2142396b4c13096e07e096152a6a85fff52de940518a61f38cab2273b8ed6b036e5c4c7c8c15d6b4bbc404a564e8a1b0562bbf65'
