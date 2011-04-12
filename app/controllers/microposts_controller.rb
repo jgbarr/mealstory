2e5f569093f65bdaff642ef7ac7a63c5f7a2cf64
Jeff Barr <jeff@jeffbarr.com>
2011-04-11 19:35:40 -0700

    added user posts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @micropost = current_user.microposts.build(params[:micropost])
--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -15,6 +15,7 @@ describe MicropostsController do
       response.should redirect_to(signin_path)
     end
   end
+
   describe "POST 'create'" do
 
     before(:each) do
@@ -59,6 +60,37 @@ describe MicropostsController do
       it "should have a flash message" do
         post :create, :micropost => @attr
         flash[:success].should =~ /micropost created/i
+      end
+    end
+  end
+  describe "DELETE 'destroy'" do
+
+    describe "for an unauthorized user" do
+
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email))
+        test_sign_in(wrong_user)
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should deny access" do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+    end
+
+    describe "for an authorized user" do
+
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @micropost = Factory(:micropost, :user => @user)
+      end
+
+      it "should destroy the micropost" do
+        lambda do 
+          delete :destroy, :id => @micropost
+        end.should change(Micropost, :count).by(-1)
       end
     end
   end
